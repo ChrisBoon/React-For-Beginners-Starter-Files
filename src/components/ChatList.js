9d1109320d0d2ed33a6c3a45de0b26ebc183b333
ChrisBoon <cboon27@gmail.com>
2016-11-03 16:44:59 -0400

    ChatList: allow teacher admin mode
    Admin mode allows teacher to see all messages, not just ones they are involved in
    
    Signed-off-by: ChrisBoon <cboon27@gmail.com>

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -8,16 +8,23 @@ class ChatList extends React.Component{
   constructor() {
     super();
     this.renderMessages = this.renderMessages.bind(this);
+    this.filterAccess = this.filterAccess.bind(this);
+    this.setViewAll = this.setViewAll.bind(this);
+    this.state = {
+      viewAll: {
+        set: false
+      }
+    }
   }
 
   renderMessages(key) {
     //all chats
     const chats = this.props.chats;
-    const myCount = this.props.users[this.props.currentUser.userId].messageHistory[key];
-    
+    const myHistory = this.props.users[this.props.currentUser.userId].messageHistory || {}
+    const myCount = myHistory[key] || 0;
+
     const unreadCount = chats[key].count - myCount;
     //limit to chats the user is involved in
-    if (chats[key].viewers.includes(this.props.currentUser.userId) ) {
       return (
         <ChatLatest
           users={this.props.users}
@@ -29,23 +36,60 @@ class ChatList extends React.Component{
           messages={chats[key].messages}
         />
       )  
+  }
+  filterAccess(key) {
+    const currentUser = this.props.currentUser,
+      chats = this.props.chats;
+
+    const isTeacher = currentUser.role === 'teacher';
+
+    if (isTeacher && this.state.viewAll.set) {
+      return true;
+    }
+    else if (chats[key].viewers.includes(currentUser.userId) ) {
+      return true;
     }
   }
+  setViewAll(event) {
+    let viewAll = {...this.state.viewAll};
+    if (event.target.checked) {
+      viewAll.set = true
+    } else {
+      viewAll.set = false
+    }
+    this.setState({viewAll})
 
+  }
 	render(){
     const chats = this.props.chats;
-    if (this.props.users[this.props.currentUser.userId].messageHistory) {
+    let teacherViewToggle;
+
+    if (this.props.currentUser.role === 'teacher') {
+      teacherViewToggle = <div className='teacher-toggle'>
+                            <label>Admin mode
+                              <input 
+                                type="checkbox" 
+                                name={teacherViewToggle}
+                                onChange={this.setViewAll}
+                                ref={(input)=>{this.checkme = input}}
+                                />
+                                </label>
+                            </div>
+    }
+    if (this.props.users[this.props.currentUser.userId].messageHistory || this.state.viewAll) {
       return(
         <div className="op2-chatList">
           <div className="chatList-controls">
             <Link to={`/new-chat`}>
               <button>New Chat</button>
             </Link>
+              {teacherViewToggle}
           </div>
           <ul>
             {
               Object
                 .keys(chats)
+                .filter(this.filterAccess)
                 .sort(
                   (a,b) => { 
                     const dateA = chats[a].messages[chats[a].messages.length-1].dateCreated,
@@ -67,9 +111,11 @@ class ChatList extends React.Component{
     } else {
       return(
         <div className="op2-chatList">
-          <div className="chatList-controls">
+          <Link to={`/new-chat`}>
             <button>New Chat</button>
-          </div>        
+          </Link>
+            {teacherViewToggle}
+
           <p className="no-chats-message">Looks like you aren't in any Chats right now.</p>
         </div>
       )
